020c7a32d77eaf1470e82864d9d20696adc54c0a
arnoheid <arno@iheidelberg.com>
2017-04-11 16:19:44 +0200

    Done uebung0

--- uebung0/uebung0/uebung0.cpp
+++ uebung0/uebung0/uebung0.cpp
@@ -6,20 +6,23 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
+#include <iterator>
 
 
-int main()
+void main()
 {
 	std::ifstream filestream("output.geom");
-	
+	std::ofstream outfile("out.dat");
 	std::string line;
+	int i = 1; //counter
 	while (std::getline(filestream,line))
 	{
-		
-
+		std::istringstream iss(line);
+		std::vector<std::string> tokens{std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>{}};
+		outfile << "v " << tokens[1] <<" "<< tokens[2] << " " << tokens[3] << "\nv " << tokens[4] << " " << tokens[5] << " " << tokens[6] << "\nv " << tokens[7] << " " << tokens[8] << " " << tokens[9] << "\nf " << i << " " << i+1 << " " << i+2<< "\n";
+		i = i + 3;
 	}
-
-
-	return 0;
+	filestream.close();
+	outfile.close();
 }
-
